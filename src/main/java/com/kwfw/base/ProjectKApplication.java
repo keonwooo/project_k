c4be74e97113af16222defcb80a6dc44bec374b4
keonwooo <rjsdn7732@naver.com>
2024-09-06 14:00:42 +0900

    [2024.09.06] -김건우 first commit

---
+++ src/main/java/com/kwfw/base/ProjectKApplication.java
@@ -0,0 +1,13 @@
+package com.kwfw.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kwfw/base/web/view/viewController.java
@@ -0,0 +1,13 @@
+package com.kwfw.base.web.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class viewController {
+
+    @GetMapping("/")
+    public String mainView() {
+        return "main/main";
+    }
+}
