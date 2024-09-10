8197bd1d126017af4c7382bb517a053457f21cf6
keonwooo <rjsdn7732@naver.com>
2024-09-10 23:54:31 +0900

    [2024.09.10] - 김건우 1. 타이핑 로직 추가

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -3,9 +3,21 @@
  */
 
 $(document).ready(function () {
-    const typingJson = {
-        wordArr: []
-        
-    };
+    const typingStr = 'This is the first paragraph of text. This is the first paragraph of text.';
+    const typingWordArr = typingStr.split(' ');
     
+    makeTyping(typingWordArr);
+    
+    $("#typingBox-area").keydown(function(e) {
+//        e.preventDefault();
+    });
 });
+
+function makeTyping(typingWordArr) {
+    typingWordArr.forEach((word) => {
+        const wordSpan = document.createElement('span');
+        wordSpan.textContent = word + ' ';
+        wordSpan.className = 'word-class';
+        $("#typingText-area").append(wordSpan);
+    });
+}
