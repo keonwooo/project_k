231603a83775f2e042d83009f22f1c36ce87c105
keonwooo <rjsdn7732@naver.com>
2024-09-15 23:55:55 +0900

    [2024.09.15] - 김건우 1. 타이핑 로직 수정

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -5,19 +5,37 @@
 $(document).ready(function () {
     const typingStr = 'This is the first paragraph of text. This is the first paragraph of text.';
     const typingWordArr = typingStr.split(' ');
+    const typingLetterArr = typingStr.split('');
     
-    makeTyping(typingWordArr);
+    typingWordArr.forEach((word) => {
+        const wordSpan = document.createElement('span');
+        wordSpan.className = 'word-class';
+        
+        typingLetterArr.forEach((letter) => {
+            const letterSpan = document.createElement('span');
+            letterSpan.textContent = letter;
+            letterSpan.className = 'letter-class';
+            
+            wordSpan.appendChild(letterSpan);
+        });
+        document.getElementById("typingText-area").appendChild(wordSpan);
+        document.getElementById("typingText-area").appendChild(document.createTextNode(' '));
+        
+    }) 
+
+    
+//    makeTyping(typingWordArr);
     
     $("#typingBox-area").keydown(function(e) {
 //        e.preventDefault();
     });
 });
 
-function makeTyping(typingWordArr) {
-    typingWordArr.forEach((word) => {
-        const wordSpan = document.createElement('span');
-        wordSpan.textContent = word + ' ';
-        wordSpan.className = 'word-class';
-        $("#typingText-area").append(wordSpan);
-    });
-}
+//function makeTyping(typingWordArr) {
+//    typingWordArr.forEach((word) => {
+//        const wordSpan = document.createElement('span');
+//        wordSpan.textContent = word + ' ';
+//        wordSpan.className = 'word-class';
+//        $("#typingText-area").append(wordSpan);
+//    });
+//}
