13d958db8a88ad13d6bdd86b1e63d99690e40d25
keonwooo <rjsdn7732@naver.com>
2024-09-16 00:25:41 +0900

    [2024.09.16] - 김건우 1. test_3

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
     const typingWordArr = typingStr.split(' ');
     const typingLetterArr = typingStr.split('');
     
+    
+    
     typingWordArr.forEach((word) => {
         const wordSpan = document.createElement('span');
         wordSpan.className = 'word-class';
