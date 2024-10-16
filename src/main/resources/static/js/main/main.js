e8fc87263e69f5c3b67f36daf68f3439b6623582
keonwooo <rjsdn7732@naver.com>
2024-10-16 22:27:17 +0900

    [2024.10.16] - 김건우 1. test_2

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -54,6 +54,7 @@ $(document).ready(function () {
 //        }
 
         const letterElements = document.querySelectorAll('#typingText-area .letter-class');
+        
         // 입력된 글자마다 처리
         typedText.split('').forEach((letter, index) => {
             if (letterElements[index]) {
