ba4d5504178fed90fbb8d372c7d635241e620a5a
keonwoo <rjsdn7732@naver.com>
2024-11-19 00:00:54 +0900

    [2024.11.19] - 김건우
    1. test_1

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -24,7 +24,6 @@ $(document).ready(function () {
         });
 
         document.getElementById("typingText-area").appendChild(wordSpan);
-        
     });
     
     const typingBox = document.getElementById("typingBox-area");
